d924f4946a9213de08d69051730a0f3802432338
Zakariya Kassim <zakariyakassim@techs-macbook-pro.local>
2016-05-05 13:47:13 +0100

    changed table style

--- Coursework Management Tool/DetailViewController.swift
+++ Coursework Management Tool/DetailViewController.swift
@@ -9,60 +9,61 @@
 import UIKit
 
 class DetailViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource{
-
-    @IBOutlet weak var levelPickerView: UIPickerView!
-    var placeHolderData: String!
     
-   var levels = ["Level 4", "Level 5", "Level 6", "Level 7"];
-
-
+  
+    
+    var placeHolderData: String!
+    var levels = ["Level 4", "Level 5", "Level 6", "Level 7"];
+    
+    
     @IBOutlet weak var btnAddCoursework: MyButton!
     @IBOutlet weak var btnSave: MyButton!
     @IBOutlet weak var txtModule: UITextField!
-    
-
     @IBOutlet weak var txtCode: UITextField!
-
+    @IBOutlet weak var levelPickerView: UIPickerView!
+    
+    
     var detailItem: Module? {
         didSet {
             // Update the view.
             self.configureView()
         }
     }
-
+    
     func configureView() {
         // Update the user interface for the detail item.
         if let detail = self.detailItem {
             if let name = self.txtModule {
                 name.text = detail.name
-                
             }
             if let code = self.txtCode {
                 code.text = detail.code
             }
         }
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         self.customTextFieldStyle(txtModule, text: "Module Name")
-                self.customTextFieldStyle(txtCode, text: "Module Code")
-self.levelPickerView.backgroundColor = UIColor.clearColor()
-
-        levelPickerView.delegate = self
-        levelPickerView.dataSource = self
+        self.customTextFieldStyle(txtCode, text: "Module Code")
+        
+        
+        self.levelPickerView.backgroundColor = UIColor.clearColor()
+        self.levelPickerView.delegate = self
+        self.levelPickerView.dataSource = self
         
         self.btnSave.backgroundColor = UIColor.clearColor()
-         self.btnAddCoursework.backgroundColor = UIColor.clearColor()
-        self.btnAddCoursework.setFillColor(UIColor(red: 0.165, green:0.427, blue:0.620, alpha:1.00))
+        self.btnSave.backgroundColor = UIColor.clearColor()
+        self.btnSave.setFillColor(UIColor(red: 0.165, green:0.427, blue:0.620, alpha:1.00))
+        self.btnSave.setRadius(15,setTopRight: 15,setBottomLeft: 15,setBottomRight: 15)
         
+        
+        self.btnAddCoursework.backgroundColor = UIColor.clearColor()
+        self.btnAddCoursework.setFillColor(UIColor(red: 0.165, green:0.427, blue:0.620, alpha:1.00))
         self.btnAddCoursework.setRadius(15,setTopRight: 15,setBottomLeft: 15,setBottomRight: 15)
         
-        self.btnSave.backgroundColor = UIColor.clearColor()
-                self.btnSave.setFillColor(UIColor(red: 0.165, green:0.427, blue:0.620, alpha:1.00))
-        
-        self.btnSave.setRadius(15,setTopRight: 15,setBottomLeft: 15,setBottomRight: 15)
+    
         
         
         self.configureView()
@@ -72,12 +73,14 @@ self.levelPickerView.backgroundColor = UIColor.clearColor()
     func customTextFieldStyle(textField: UITextField, text: String) {
         
         textField.backgroundColor = UIColor.clearColor()
-        textField.attributedPlaceholder = NSAttributedString(string: text,
-                                                               attributes:[NSForegroundColorAttributeName: UIColor.grayColor()])
-        textField.layer.cornerRadius = 8;
-        textField.layer.borderWidth = 2;
+        textField.attributedPlaceholder = NSAttributedString(string: text, attributes:[NSForegroundColorAttributeName: UIColor.grayColor()])
+        textField.layer.cornerRadius = 8
+        textField.layer.borderWidth = 2
         textField.layer.borderColor = UIColor(red: 0.165, green:0.427, blue:0.620, alpha:1.00).CGColor
         
+        levelPickerView.layer.borderWidth = 2
+        levelPickerView.layer.borderColor = UIColor(red: 0.165, green:0.427, blue:0.620, alpha:1.00).CGColor
+        levelPickerView.layer.cornerRadius = 8
     }
     
     func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
@@ -92,23 +95,23 @@ self.levelPickerView.backgroundColor = UIColor.clearColor()
         var attributedString: NSAttributedString!
         
         
-            attributedString = NSAttributedString(string: levels[row], attributes: [NSForegroundColorAttributeName : UIColor(red: 0.165, green:0.427, blue:0.620, alpha:1.00)])
+        attributedString = NSAttributedString(string: levels[row], attributes: [NSForegroundColorAttributeName : UIColor(red: 0.165, green:0.427, blue:0.620, alpha:1.00)])
         
         return attributedString
     }
-
+    
     func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         
-            print(levels[row])
+        print(levels[row])
         
     }
-
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
--- Coursework Management Tool/MasterViewController.swift
+++ Coursework Management Tool/MasterViewController.swift
@@ -9,55 +9,58 @@
 import UIKit
 
 class MasterViewController: UITableViewController {
-
+    
     var detailViewController: DetailViewController? = nil
     var objects = [AnyObject]()
-
+    
     var modules = [Module]()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.navigationItem.leftBarButtonItem = self.editButtonItem()
-
+        
         let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: #selector(MasterViewController.insertNewCourse(_:)))
         
-       // let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewCourse:")
+        
+        
+        // let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewCourse:")
         
         self.navigationItem.rightBarButtonItem = addButton
         if let split = self.splitViewController {
             let controllers = split.viewControllers
             self.detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
         }
+        self.view.backgroundColor = UIColor.blackColor()
+        self.tableView.separatorStyle = UITableViewCellSeparatorStyle.None
         
         
-        // cell.backgroundColor = UIColor.yellowColor()
         
         
         
-     //   self.courses = [Course(name: "Computer Science"), Course(name: "Software Engineering"), Course(name: "Multimedia")]
+        //   self.courses = [Course(name: "Computer Science"), Course(name: "Software Engineering"), Course(name: "Multimedia")]
         
         
     }
-
+    
     override func viewWillAppear(animated: Bool) {
         self.clearsSelectionOnViewWillAppear = self.splitViewController!.collapsed
         super.viewWillAppear(animated)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     func insertNewCourse(sender: AnyObject) {
         modules.insert(Module(name:  "New Module", code: "0000"), atIndex: 0)
         let indexPath = NSIndexPath(forRow: 0, inSection: 0)
         self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
     }
-
+    
     // MARK: - Segues
-
+    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "showDetail" {
             if let indexPath = self.tableView.indexPathForSelectedRow {
@@ -66,33 +69,34 @@ class MasterViewController: UITableViewController {
                 controller.detailItem = course
                 controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
                 controller.navigationItem.leftItemsSupplementBackButton = true
+                
             }
         }
     }
-
+    
     // MARK: - Table View
-
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return modules.count
     }
-
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
-
+        
         let course = modules[indexPath.row]
         cell.textLabel!.text = course.name
         return cell
     }
-
+    
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-
+    
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
             modules.removeAtIndex(indexPath.row)
@@ -101,7 +105,20 @@ class MasterViewController: UITableViewController {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
         }
     }
-
-
+    
+    override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+        let backgroundView = UIView()
+        backgroundView.backgroundColor = UIColor(red: 0.021, green:0.055, blue:0.081, alpha:1.00)
+        
+        cell.backgroundColor = UIColor.clearColor()
+        cell.textLabel?.textColor = UIColor(red: 0.165, green:0.427, blue:0.620, alpha:1.00)
+        cell.textLabel?.font = UIFont(name:"Calibri-Light", size: 50.0)
+        cell.selectedBackgroundView = backgroundView
+        
+    }
+    
+    
+    
 }
 
